ba0556ccbbf9c35083e4232b294d1a5f36c847a3
Pratheeksha KA <pratheekshaka@gmail.com>
2026-06-15 20:52:41 +0530

    Fixed login issue

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,13 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+/* ==========================
+   REGISTER USER
+========================== */
+
 const registerUser = async (req, res) => {
+
   try {
 
     const { name, email, password } = req.body;
@@ -27,14 +36,18 @@ const registerUser = async (req, res) => {
     });
 
     const token = jwt.sign(
+
       {
         id: user._id,
         role: user.role
       },
+
       process.env.JWT_SECRET,
+
       {
         expiresIn: "7d"
       }
+
     );
 
     res.status(201).json({
@@ -62,4 +75,91 @@ const registerUser = async (req, res) => {
     });
 
   }
+
+};
+
+/* ==========================
+   LOGIN USER
+========================== */
+
+const loginUser = async (req, res) => {
+
+  try {
+
+    const { email, password } = req.body;
+
+    const user =
+    await User.findOne({ email });
+
+    if (!user) {
+
+      return res.status(400).json({
+        message: "Invalid Credentials"
+      });
+
+    }
+
+    const isMatch =
+    await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!isMatch) {
+
+      return res.status(400).json({
+        message: "Invalid Credentials"
+      });
+
+    }
+
+    const token = jwt.sign(
+
+      {
+        id: user._id,
+        role: user.role
+      },
+
+      process.env.JWT_SECRET,
+
+      {
+        expiresIn: "7d"
+      }
+
+    );
+
+    res.status(200).json({
+
+      token,
+
+      user: {
+
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role
+
+      }
+
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      message: error.message
+    });
+
+  }
+
+};
+
+/* ==========================
+   EXPORTS
+========================== */
+
+module.exports = {
+
+  registerUser,
+  loginUser
+
 };
