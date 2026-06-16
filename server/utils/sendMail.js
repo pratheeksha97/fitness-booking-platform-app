c9965e2ea3669c0d9061adbc7e2fe1b9ba34f1fb
Pratheeksha KA <pratheekshaka@gmail.com>
2026-06-16 15:22:00 +0530

    Invoice issue debug

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -2,58 +2,79 @@ const axios = require("axios");
 const fs = require("fs");
 
 const sendMail = async (options) => {
+
+  const payload = {
+
+    sender: {
+      name: "Fitness Booking Platform",
+      email: "pratheekshaka@gmail.com"
+    },
+
+    to: [
+      {
+        email: options.email
+      }
+    ],
+
+    subject: options.subject,
+
+    htmlContent: options.html
+
+  };
+
+  if (
+    options.attachments &&
+    options.attachments.length > 0
+  ) {
+    console.log(
+    "Invoice Path:",
+    options.attachments[0].path
+  );
+
+  console.log(
+    "Invoice Exists:",
+    fs.existsSync(
+      options.attachments[0].path
+    )
+  );
+
+
+    payload.attachment =
+    options.attachments.map(file => ({
+
+      name: file.filename,
+
+      content: fs
+        .readFileSync(file.path)
+        .toString("base64")
+
+    }));
+
+  }
+
   try {
 
-    const payload = {
-      sender: {
-        name: "Fitness Booking Platform",
-        email: "pratheekshaka@gmail.com"
-      },
+    console.log(
+      "Payload Sent:",
+      JSON.stringify(payload, null, 2)
+    );
 
-      to: [
-        {
-          email: options.email
-        }
-      ],
+    const response =
+    await axios.post(
 
-      subject: options.subject,
-
-      htmlContent: options.html
-    };
-
-    // Add attachments only if they exist
-    if (
-      options.attachments &&
-      options.attachments.length > 0
-    ) {
-      
-      console.log("Attachment Path:", file.path);
-console.log(
-  "File Exists:",
-  fs.existsSync(file.path)
-);
-      payload.attachment =
-        options.attachments.map(file => ({
-
-          name: file.filename,
-
-          content: fs
-            .readFileSync(file.path)
-            .toString("base64")
-
-        }));
-
-    }
-
-    const response = await axios.post(
       "https://api.brevo.com/v3/smtp/email",
+
       payload,
+
       {
         headers: {
-          "api-key": process.env.BREVO_API_KEY,
-          "Content-Type": "application/json"
+          "api-key":
+          process.env.BREVO_API_KEY,
+          "Content-Type":
+          "application/json"
         }
       }
+
     );
 
     console.log(
@@ -66,26 +87,18 @@ console.log(
   } catch (error) {
 
     console.error(
-  "Brevo Full Error:",
-  JSON.stringify(
-    error.response?.data,
-    null,
-    2
-  )
-);
-
-console.error(
-  "Payload Sent:",
-  JSON.stringify(
-    payload,
-    null,
-    2
-  )
-);
+      "Brevo Full Error:",
+      JSON.stringify(
+        error.response?.data,
+        null,
+        2
+      )
+    );
 
     throw error;
 
   }
+
 };
 
 module.exports = sendMail;
